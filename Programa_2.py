5293c4d52467630e47fbdb122eca869c1f37a1fd
Carlos <57544836+carmoran0@users.noreply.github.com>
2022-09-30 10:33:51 +0200

    Update Programa_2.py

--- Programa_2.py
+++ Programa_2.py
@@ -1,20 +1,22 @@
 # Segundo (y tercer) día de clase, calculador de media
-import statistics
-
-print("MEDIUM CALCULATUM")
-n1 = float(input("Numero1: "))
-n2 = float(input("Numero2: "))
-n3 = float(input("Numero3: "))
-print(statistics.median([n1, n2, n3]))
-if statistics.median([n1, n2, n3]) < 5:
-    print("Suspendido")
-    nombre = input("¿Como te llamas? ")
-    if nombre == "Carlos":
-        print("entonces aprobado :)")
+def calcumadora():
+    import statistics
+    
+    print("MEDIUM CALCULATUM")
+    n1 = float(input("Numero1: "))
+    n2 = float(input("Numero2: "))
+    n3 = float(input("Numero3: "))
+    print(statistics.median([n1, n2, n3]))
+    if statistics.median([n1, n2, n3]) < 5:
+        print("Suspendido")
+        nombre = input("¿Como te llamas? ")
+        if nombre == "Carlos":
+            print("entonces aprobado :)")
+        else:
+            print("pues te quedas con tu" , statistics.median([n1, n2, n3] ))
     else:
-        print("pues te quedas con tu" , statistics.median([n1, n2, n3] ))
-else:
-    print("Aprobado")
-gon = input ()
-if gon == ("GONZALO TOBAJAS"):
-    print("Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtag")
+        print("Aprobado")
+    gon = input ()
+    if gon == ("GONZALO TOBAJAS"):
+        print("Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtag")
+calcumadora()
